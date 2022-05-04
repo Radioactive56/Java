6ed0b90dd8e1fc320b2ccf6163960155536692f8
Manav Dhruve <87529123+Radioactive56@users.noreply.github.com>
2022-05-04 19:01:27 +0530

    Add files via upload

---
+++ PRACTICAL/CharDataType.java
@@ -0,0 +1,12 @@
+
+public class CharDataType {
+
+	public static void main(String[] args) {
+		char marks, grade;
+		marks= '8';
+		grade = 'B';
+		System.out.println("Marks:" +marks);
+		System.out.println("Grade:" +grade);
+	}
+
+}
