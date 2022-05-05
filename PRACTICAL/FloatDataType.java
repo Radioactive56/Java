addf9fad16183d33c27803557d9543841437bed0
Manav Dhruve <87529123+Radioactive56@users.noreply.github.com>
2022-05-05 17:37:38 +0530

    Add files via upload

---
+++ PRACTICAL/FloatDataType.java
@@ -0,0 +1,15 @@
+
+public class FloatDataType {
+
+	public static void main(String[] args) {
+		float myfloat1, myfloat2, result; 
+		myfloat1 = 1000.666f; 
+		myfloat2 = 110.77f; 
+		result = myfloat1 - myfloat2; 
+		System.out.println("Number 1: " +myfloat1); 
+		System.out.println("Number 2: " +myfloat2); 
+	    System.out.println("Number 1 - Number 2: " +result);
+	}
+
+	}
+
