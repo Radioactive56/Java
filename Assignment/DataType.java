21a18a29807f9265337e067c69b0315f7faf8a26
Manav Dhruve <87529123+Radioactive56@users.noreply.github.com>
2022-05-05 18:06:55 +0530

    Add files via upload

---
+++ Assignment/DataType.java
@@ -0,0 +1,23 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		char grade='B';
+		int marks=67;
+		byte srno=12;
+		short rollno=3500;
+		long pid=197019876L;
+		float avg=35.89f;
+		double phone=98765431234d;
+		boolean mybool=true;
+		System.out.println("Grade:" +grade);
+		System.out.println("marks:" +marks);
+		System.out.println("srno:" +srno);
+		System.out.println("rollno:" +rollno);
+		System.out.println("pid:" +pid);
+		System.out.println("avg:" +avg);
+		System.out.println("phone:" +phone);
+		System.out.println("boolean value is :" +mybool);
+	}
+
+}
